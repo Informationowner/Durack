afb313c0cfb5941a84b81a636b99e6a8c5f7e0ea
Hernantos <sattarov-artur@mail.ru>
2018-12-22 13:25:12 +0300

    Initial commit

---
+++ Classes/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Durack.Classes
+{
+    class Card
+    {
+        public Suit Suit { get;protected set; }
+        public Rank Rank { get;protected set; }
+        public bool IsTrump { get;protected set;}
+
+        public override bool Equals(object obj)
+        {
+            if (((Card)obj).IsTrump && IsTrump && ((Card)obj).Suit == Suit && ((Card)obj).Rank == Rank)
+                return true;
+            else return false;
+        }
+
+        public bool Comparable(Card enemy)
+        {
+            if (this.IsTrump || enemy.IsTrump)
+                return true;
+            if (this.Suit == enemy.Suit)
+                return true;
+
+            return false;
+        }
+
+        public int Compare(Card enemy)
+        {
+            
+
+        }
+
+    }
+}
